c3e122ac87e1aab9f048bc4b9dcc618bf24f3c20
junma <junma0704@gmail.com>
2021-10-27 12:08:51 -0700

    make server serve public dir as a static & resolve trailing whitespace

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello World from Server/index.js!')
-})
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 app.listen(port, () => {
-  console.log(`app listening at http://localhost:${port}`)
-})
+  console.log(`app listening at http://localhost:${port}`);
+});
