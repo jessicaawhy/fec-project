a7ad765b82a288fef49387e6a9b97c705445d934
Jessica Yang <jessicaawhy@gmail.com>
2021-10-29 16:52:54 -0700

    Restructure app and initialize product to hardcoded id

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import { ProductProvider } from './components/ProductContext';
+import { ProductProvider } from './ProductContext';
 
 render(
   <ProductProvider>
