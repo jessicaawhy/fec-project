217663bae7961c1e2ec75ad08717a89f46435196
junma <junma0704@gmail.com>
2021-11-02 16:20:37 -0700

    added test for MasterQA component

--- client/components/QuestionsAndAnswers/MasterQA.test.js
+++ client/components/QuestionsAndAnswers/MasterQA.test.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import { render, screen} from '@testing-library/react';
-import AnswerItem from './AnswerItem';
+import MasterQA from './MasterQA';
+
+it("should render MasterQA component correctly", () => {
+  const { getByTestId } = render(<MasterQA />);
+  expect(getByTestId('masterQA')).toBeInTheDocument();
+});
+
+// it('should display initial MasterQA UI', () => {
+//   render(<MasterQA />);
+//   expect(screen.getByRole('heading', { name: /Questions & Answers/ })).toBeInTheDocument();
+// });
