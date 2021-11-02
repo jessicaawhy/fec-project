e24da45e3fda9037ecb740f0c3f2eee6651fbcc4
Jessica Yang <jessicaawhy@gmail.com>
2021-11-02 10:04:36 -0700

    Display 2 reviews at a time

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+// import ReviewsComponent from './Reviews/ReviewsComponent';
 
 const App = () => (
   <>
     {/* place main module components within this fragment */}
     <h1>7 Satsumas</h1>
+    {/* <ReviewsComponent /> */}
   </>
 );
 
--- client/components/Reviews/Reviews.jsx
+++ client/components/Reviews/Reviews.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ReviewsList from './ReviewsList';
 
-const Reviews = ({ reviews }) => (
-  <div data-testid="reviews">
-    <div>
-      {`${reviews.length} reviews, sorted by `}
-      <button type="button">relevance</button>
+const Reviews = ({ reviews }) => {
+  const [num, setNum] = useState(2);
+
+  const updateNum = () => {
+    if (num + 2 < reviews.length) {
+      setNum(num + 2);
+    } else {
+      setNum(reviews.length);
+    }
+  };
+
+  return (
+    <div data-testid="reviews">
+      <div>
+        {`${reviews.length} reviews, sorted by `}
+        <button type="button">relevance</button>
+      </div>
+      <ReviewsList reviews={reviews.filter((review, i) => i < num)} />
+      {
+        num < reviews.length
+        && <button type="button" onClick={updateNum}>MORE REVIEWS</button>
+      }
+      <button type="button">ADD A REVIEW</button>
     </div>
-    <ReviewsList reviews={reviews} />
-    <button type="button">MORE REVIEWS</button>
-    <button type="button">ADD A REVIEW</button>
-  </div>
-);
+  );
+};
 
 export default Reviews;
 
