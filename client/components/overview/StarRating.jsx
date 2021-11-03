292470b2f4670c2edcded32e6e50f5a10d38746b
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-03 09:35:23 -0700

    Renders stars to DOM

--- client/components/overview/StarRating.jsx
+++ client/components/overview/StarRating.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Star from './Star';
 
 const StarRating = () => (
-  <div data-testid="starRating">StarRating rendering...</div>
+  <div data-testid="starRating">
+    <Star />
+    <Star />
+    <Star />
+    <Star />
+    <Star />
+  </div>
 );
 
 export default StarRating;
