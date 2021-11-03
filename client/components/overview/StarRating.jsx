e4e6098f7e9ca9dc358095bd364f8148abf9ce4d
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-03 11:38:19 -0700

    Implements star asset and Read All Reviews link; aligns in one row

--- client/components/overview/StarRating.jsx
+++ client/components/overview/StarRating.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import Star from './Star';
 
 const StarRating = () => (
   <div data-testid="starRating">
-    <Star />
-    <Star />
-    <Star />
-    <Star />
-    <Star />
+    <StarRatingStyle>
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+      {/* change href="#starRating" to reference Reviews module */}
+      <a href="#starRating">Read All Reviews</a>
+    </StarRatingStyle>
   </div>
 );
 
 export default StarRating;
+
+const StarRatingStyle = styled.div`
+  display: flex;
+  flex-direction: row
+`;
