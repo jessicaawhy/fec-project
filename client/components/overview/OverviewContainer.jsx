50424b09c8da2e409700428cfb8012e0e7573077
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-03 11:36:41 -0700

    Passes products, styles, and id to subcomponents; StarRating props  needs revision

--- client/components/overview/OverviewContainer.jsx
+++ client/components/overview/OverviewContainer.jsx
@@ -5,16 +5,18 @@ import ProductDescription from './ProductDescription';
 import ImageGallery from './ImageGallery';
 import StyleSelector from './StyleSelector';
 import AddToCart from './AddToCart';
-// import ProductContext from '../../ProductContext';
+import products from '../testData/testProducts';
+import styles from '../testData/testStyles';
+import id from '../testData/testId';
 
 const OverviewContainer = () => (
   <div data-testid="container">
-    <StarRating />
-    <ProductInfo />
-    <ProductDescription />
-    <ImageGallery />
-    <StyleSelector />
-    <AddToCart />
+    <StarRating products={products} />
+    <ImageGallery styles={styles} />
+    <ProductInfo styles={styles} id={id} />
+    <ProductDescription id={id} />
+    <StyleSelector styles={styles} />
+    <AddToCart styles={styles} />
   </div>
 );
 
