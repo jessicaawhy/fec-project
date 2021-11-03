e622c2da438b12a820035e8af699a4e764f5f673
Jessica Yang <jessicaawhy@gmail.com>
2021-11-02 18:59:12 -0700

    Update reviews to allow for sorting

--- client/components/Reviews/Reviews.jsx
+++ client/components/Reviews/Reviews.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import ReviewsList from './ReviewsList';
 
 const Reviews = ({ reviews }) => {
   const [num, setNum] = useState(2);
+  const [sort, setSort] = useState(0);
+  const [orderedSubset, setOrderedSubset] = useState([]);
 
   const updateNum = () => {
     if (num + 2 < reviews.length) {
@@ -13,13 +15,60 @@ const Reviews = ({ reviews }) => {
     }
   };
 
+  const handleChange = (e) => {
+    setSort(Number(e.target.value));
+  };
+
+  const sortHelpful = (reviewsArray) => {
+    const copy = reviewsArray.slice();
+
+    copy.sort((a, b) => b.helpfulness - a.helpfulness);
+
+    return copy;
+  };
+
+  const sortNewest = (reviewsArray) => {
+    const copy = reviewsArray.slice();
+
+    copy.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    return copy;
+  };
+
+  const sortRelevant = (reviewsArray) => {
+    const copy = reviewsArray.slice();
+
+    copy.sort((a, b) => new Date(b.date) - new Date(a.date));
+    copy.sort((a, b) => b.helpfulness - a.helpfulness);
+
+    return copy;
+  };
+
+  const sortReviews = (reviewsArray, option) => {
+    if (option === 0) {
+      return sortRelevant(reviewsArray);
+    } if (option === 1) {
+      return sortHelpful(reviewsArray);
+    }
+    return sortNewest(reviewsArray);
+  };
+
+  useEffect(() => {
+    setOrderedSubset(sortReviews(reviews, sort));
+    setNum(2);
+  }, [sort]);
+
   return (
     <div data-testid="reviews">
       <div>
         {`${reviews.length} reviews, sorted by `}
-        <button type="button">relevance</button>
+        <select name="sort" id="sort-options" onChange={handleChange}>
+          <option value="0">Relevant</option>
+          <option value="1">Helpful</option>
+          <option value="2">Newest</option>
+        </select>
       </div>
-      <ReviewsList reviews={reviews.filter((review, i) => i < num)} />
+      <ReviewsList reviews={orderedSubset.filter((review, i) => i < num)} />
       {
         num < reviews.length
         && <button type="button" onClick={updateNum}>MORE REVIEWS</button>
