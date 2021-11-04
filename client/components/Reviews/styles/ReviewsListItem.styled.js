b5088fab6668b689b326c9b539ee20f6f3b0443f
Jessica Yang <jessicaawhy@gmail.com>
2021-11-03 23:22:46 -0700

    Refactor ReviewsListItem and related items

--- client/components/Reviews/styles/ReviewsListItem.styled.js
+++ client/components/Reviews/styles/ReviewsListItem.styled.js
@@ -1,31 +1,72 @@
 import styled from 'styled-components';
+import Flex from '../../styles/Flex.styled';
+import ButtonLink from '../../styles/ButtonLink.styled';
 
 export const Item = styled.div`
+  margin: 10px 0;
   > * {
-    padding: 10px;
+    margin: 10px 0;
   };
 `;
+
+/* Header */
 
 export const Header = styled.div`
   display: flex;
   justify-content: space-between;
+
+  ${Flex} {
+    > span {
+      padding: 0 5px;
+      color: rgb(100, 100, 100);
+    }
+  }
 `;
 
-export const HeaderDetail = styled.div`
-  > span {
-    padding: 4px;
-  };
-`;
+/* Body */
 
 export const Summary = styled.div`
   font-weight: bold;
 `;
 
-export const Body = styled.div`
+export const Response = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: rgb(240, 240, 240);
+  padding: 15px;
+  > * {
+    &:first-child {
+      font-weight: bold;
+    }
+  }
 `;
 
+export const ImgIcon = styled.img`
+  margin: 0 10px;
+  height: 75px;
+`;
+
+/* Footer */
+
 export const Footer = styled.div`
-  > * {
-    font-size: inherit;
+  display: flex;
+
+  ${Flex} {
+    > * {
+      &:first-child {
+        padding-right: 10px;
+      }
+      &:nth-child(2) {
+        padding-right: 5px;
+      }
+    }
+  };
+
+  & .separator {
+    padding: 0 10px;
   }
+
+  ${ButtonLink} {
+    font-size: inherit;
+  };
 `;
--- client/components/styles/Global.js
+++ client/components/styles/Global.js
@@ -5,6 +5,8 @@ const GlobalStyles = createGlobalStyle`
 
   * {
     font-family: 'Open Sans', sans-serif;
+    margin: 0;
+    padding: 0;
   }
 `;
 
