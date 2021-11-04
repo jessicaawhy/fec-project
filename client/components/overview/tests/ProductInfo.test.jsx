defee258f04accf4955f8ebc2155a2f2711aae59
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-04 15:31:29 -0700

    Revises product info tests

--- client/components/overview/tests/ProductInfo.test.jsx
+++ client/components/overview/tests/ProductInfo.test.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ProductInfo from '../ProductInfo';
+import id from './testData/testId';
 
 it('should display testid from ProductInfo', () => {
   const { getByTestId } = render(<ProductInfo />);
   expect(getByTestId('productInfo')).toBeInTheDocument();
+});
+
+it('should render the NAME of a product', () => {
+  render(<ProductInfo />);
+  // how to render dynamic data here for the name?
+  const productName = screen.getByText(`${id.name}`);
+  expect(productName).toBeInTheDocument();
+});
+
+it('should render the CATEGORY of a product', () => {
+  render(<ProductInfo />);
+  // how to render dynamic data here for the category?
+  const productCategory = screen.getByText(`${id.category}`);
+  expect(productCategory).toBeInTheDocument();
+});
+
+it('should render the PRICE of a product', () => {
+  render(<ProductInfo />);
+  // how to render dynamic data here for the price?
+  const productPrice = screen.getByText(`${id.category}`);
+  expect(productPrice).toBeInTheDocument();
 });
