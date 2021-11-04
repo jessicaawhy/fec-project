4cf28fa8c343ffe02fbf9904a998a5293a46d7b1
Jessica Yang <jessicaawhy@gmail.com>
2021-11-03 18:28:34 -0700

    Create global style file and refactor reusable styles out

--- client/components/Reviews/SummaryRatingItem.jsx
+++ client/components/Reviews/SummaryRatingItem.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ButtonLink from '../styles/ButtonLink.styled';
 import {
-  RatingContainer, ButtonLink, ProgressBorder, ProgressBar,
+  RatingContainer, ProgressBorder, ProgressBar,
 } from './styles/SummaryRatingItem.styled';
 
 const SummaryRatingItem = ({
---
+++ client/components/styles/Global.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap');
+
+  * {
+    font-family: 'Open Sans', sans-serif;
+  }
+`;
+
+export default GlobalStyles;
