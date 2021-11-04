1e169f0a706441ab09fcc200e6d4e31119c67d39
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-04 16:34:58 -0700

    Adds base styling to AddToCart

--- client/components/overview/AddToCart.jsx
+++ client/components/overview/AddToCart.jsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import map from 'lodash/map';
+import { CartContainer, CartButton, DropDown } from './styles/AddToCart.style';
 
 const AddToCart = ({ styles }) => (
-  <div data-testid="addToCart">
-    {/*
-      // Add To Cart Button
-      // Size drop down
-      // Quantity drop down
-    */}
-    <button type="submit">Add To Cart</button>
-    <select name="sizes" id="sizes">
+  <CartContainer data-testid="addToCart">
+    <CartButton type="submit">Add To Cart</CartButton>
+    <DropDown name="sizes" id="sizes">
       {styles.results.map((style) => (
         map(style.skus, (sku) => (
           // does not yet reflect corresponding size/quantity data
           <option value={`${sku}`}>{sku.size}</option>
         ))
       ))}
-    </select>
-    <select name="quantity" id="quantity">
+    </DropDown>
+    <DropDown name="quantity" id="quantity">
       {styles.results.map((style) => (
         map(style.skus, (sku) => (
           // does not yet reflect corresponding size/quantity data
           <option value={`${sku}`}>{sku.quantity}</option>
         ))
       ))}
-    </select>
-  </div>
+    </DropDown>
+  </CartContainer>
 );
 
 export default AddToCart;
