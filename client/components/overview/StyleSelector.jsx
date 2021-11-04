5715a424ab9286b230b445622808f13b7fa3d036
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-04 15:37:12 -0700

    Adds styling for style selector

--- client/components/overview/StyleSelector.jsx
+++ client/components/overview/StyleSelector.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import map from 'lodash/map';
 import StyleButton from './StyleButton';
+import { StyleSelectorContainer } from './styles/StyleSelector.style';
 
 const StyleSelector = ({ styles }) => (
-  <div data-testid="styleSelector">
+  <StyleSelectorContainer className="style-selector">
     {styles.results.map((style) => (
-      <StyleButton style={style} />
+      map(style.photos, (photo) => (
+        <StyleButton photo={photo} />
+      ))
     ))}
-  </div>
+  </StyleSelectorContainer>
 );
 
 export default StyleSelector;
-
 StyleSelector.propTypes = PropTypes.shape({
   products: PropTypes.arrayOf(PropTypes.object),
   styles: PropTypes.objectOf(PropTypes.any),
