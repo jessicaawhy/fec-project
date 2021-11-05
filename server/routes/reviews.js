5a4f87f3f4adc9acb2a7b6c5fc66007982a5379f
Jessica Yang <jessicaawhy@gmail.com>
2021-11-04 23:16:36 -0700

    Upddate reviews route file to include sort param

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -3,7 +3,7 @@ const reviewsController = require('../controllers/reviewsController');
 
 const router = express.Router();
 
-router.get('/:product_id/:page/:count', reviewsController.review_get);
+router.get('/:product_id/:page/:count/:sort', reviewsController.review_get);
 router.get('/meta/:product_id', reviewsController.review_get_meta);
 
 module.exports = router;
