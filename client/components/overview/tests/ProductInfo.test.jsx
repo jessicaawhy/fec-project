20bee879d700a71d4fae7c876df2409a14ebd5ab
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-05 09:00:23 -0700

    Adds test coverage for Product Info text elements

--- client/components/overview/tests/ProductInfo.test.jsx
+++ client/components/overview/tests/ProductInfo.test.jsx
@@ -5,27 +5,24 @@ import ProductInfo from '../ProductInfo';
 import id from './testData/testId';
 
 it('should display testid from ProductInfo', () => {
-  const { getByTestId } = render(<ProductInfo />);
+  const { getByTestId } = render(<ProductInfo id={id} />);
   expect(getByTestId('productInfo')).toBeInTheDocument();
 });
 
 it('should render the NAME of a product', () => {
-  render(<ProductInfo />);
-  // how to render dynamic data here for the name?
+  render(<ProductInfo id={id} />);
   const productName = screen.getByText(`${id.name}`);
   expect(productName).toBeInTheDocument();
 });
 
 it('should render the CATEGORY of a product', () => {
-  render(<ProductInfo />);
-  // how to render dynamic data here for the category?
+  render(<ProductInfo id={id} />);
   const productCategory = screen.getByText(`${id.category}`);
   expect(productCategory).toBeInTheDocument();
 });
 
 it('should render the PRICE of a product', () => {
-  render(<ProductInfo />);
-  // how to render dynamic data here for the price?
+  render(<ProductInfo id={id} />);
   const productPrice = screen.getByText(`${id.category}`);
   expect(productPrice).toBeInTheDocument();
 });
