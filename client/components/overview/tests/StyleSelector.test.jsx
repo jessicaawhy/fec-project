2ef6ccdeab11c940a46568b2e6b8313693da34a5
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-05 16:27:20 -0700

    Add tests to render StyleSelector component and button to select a style

--- client/components/overview/tests/StyleSelector.test.jsx
+++ client/components/overview/tests/StyleSelector.test.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import StyleSelector from '../StyleSelector';
+import StyleButton from '../StyleButton';
+import styles from './testData/testStyles';
 
-it('should display testid from StyleSelector', () => {
-  const { getByTestId } = render(<StyleSelector />);
-  expect(getByTestId('styleSelector')).toBeInTheDocument();
+it('should render StyleSelector component', () => {
+  render(<StyleSelector styles={styles} />);
+
+  const container = screen.getByTestId('style-selector');
+
+  expect(container).toHaveClass('style-selector');
+});
+
+it('should render StyleButton component', () => {
+  render(<StyleButton photo={styles.results[0].photos[0].thumbnail_url} />);
+
+  const button = screen.getByTestId('style-button');
+
+  expect(button).toHaveClass('style-button');
 });
