1f893f65b606729ff24f1004c173c409d68b0648
Jessica Yang <jessicaawhy@gmail.com>
2021-11-04 23:05:45 -0700

    Create server routes handlers for reviews get requests

---
+++ server/controllers/reviewsController.js
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+const axios = require('axios');
+
+exports.review_get = (req, res) => {
+  const { product_id, page, count } = req.params;
+
+  const config = {
+    headers: { Authorization: process.env.TOKEN },
+  };
+
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/reviews/?${new URLSearchParams({
+    product_id,
+    page,
+    count,
+  })}`, config)
+    .then((response) => response.data)
+    .then((data) => res.send(data));
+};
+
+exports.review_get_meta = (req, res) => {
+  const { product_id } = req.params;
+
+  const config = {
+    headers: { Authorization: process.env.TOKEN },
+  };
+
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/reviews/meta/?${new URLSearchParams({
+    product_id,
+  })}`, config)
+    .then((response) => response.data)
+    .then((data) => res.send(data));
+};
