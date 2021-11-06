f30bf454df2aba45230d35534f65c300ef0173b3
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-06 01:35:34 -0700

    Format API data

--- server/helpers/dataTransforms.js
+++ server/helpers/dataTransforms.js
@@ -1,46 +1,38 @@
-import map from 'lodash/map';
+const forEach = require('lodash/forEach');
 
-module.exports = {
-  // when the data comes back from the api call for STYLES
-  // create const styles as an empty array
-  // iterate through styles.results
-  // create empty style object
-  // style.style_id = style_id
-  // style.name = name
+exports.formatStyles = (styles) => {
+  const productStyles = [];
 
-  formatStyles: (styles) => {
-    const productStyles = [];
+  for (let i = 0; i < styles.results.length; i += 1) {
+    const style = {};
+    let count = 0;
 
-    for (let i = 0; i < styles.results; i += 1) {
-      const style = {};
+    style.style_id = styles.results[i].style_id;
+    style.name = styles.results[i].name;
+    style.original_price = styles.results[i].original_price;
+    style.sale_price = styles.results[i].sale_price;
+    style.default = styles.results[i]['default?'];
+    style.skus = [];
+    style.photos = [];
 
-      style.style_id = styles.results[i].style_id;
-      style.name = styles.results[i].name;
-      style.original_price = styles.results[i].original_price;
-      style.sale_price = styles.results[i].sale_price;
-      style.default = styles.results[i]['default?'];
-      style.skus = [];
-      style.photos = [];
+    forEach(styles.results[i].skus, (sku) => {
+      const skuObject = {};
+      skuObject.sku = Number(Object.keys(styles.results[i].skus)[count]);
+      skuObject.size = sku.size;
+      skuObject.quantity = sku.quantity;
+      count += 1;
+      style.skus.push(skuObject);
+    });
 
-      map(styles.results[i].skus, (sku) => {
-        const skuObject = {};
-        skuObject.sku = sku;
-        skuObject.size = sku.size;
-        skuObject.quantity = sku.quantity;
+    forEach(styles.results[i].photos, (photo) => {
+      const photoObject = {};
+      photoObject.thumbnail_url = photo.thumbnail_url;
+      photoObject.url = photo.url;
+      style.photos.push(photoObject);
+    });
 
-        styles.skus.push(skuObject);
-      });
+    productStyles.push(style);
+  }
 
-      map(styles.results[i].photos, (photo) => {
-        const photoObject = {};
-
-        photoObject.thumbnail_url = photo.thumbnail_url;
-        photoObject.url = photo.url;
-
-        styles.photos.push(photoObject.skus);
-      });
-
-      productStyles.push(style);
-    }
-  },
+  return productStyles;
 };
