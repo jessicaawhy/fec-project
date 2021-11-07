20341091d49c7be406d42ace4924d6e45970d6df
junma <junma0704@gmail.com>
2021-11-07 00:07:04 -0700

    Apply axios defaults for baseURL and Header

--- server/controllers/questionsController.js
+++ server/controllers/questionsController.js
@@ -1,29 +1,33 @@
 const axios = require('axios');
+
+axios.defaults.baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/qa/questions';
+axios.defaults.headers.common.Authorization = process.env.TOKEN;
 // apply default header
 
-exports.getQuestions = (req, res) => {
-  console.log('------', req);
+const getQuestions = (req, res) => {
   const { product_id, page, count } = req.params;
-  // const config = {
-  //   headers: { Authorization: process.env.TOKEN },
-  // };
+  console.log(req.url);
   axios({
     method: 'get',
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/qa/questions/?${new URLSearchParams({
-      product_id,
-      page,
-      count,
-    })}`,
-    headers: { Authorization: process.env.TOKEN },
+    url: `/?${new URLSearchParams({ product_id, page, count })}`,
+    // url: `/?product_id=${product_id}&page=${page}&count=${count}`,
   })
     .then((response) => response.data)
     .then((data) => res.send(data))
     .catch((err) => {
-      console.log('---------', err);
+      console.log('---------', err.request);
       res.send(500);
     });
 };
 
-// module.exports = {
-//   getQuestions,
-// };
+module.exports = {
+  getQuestions,
+};
+/** TO BE DELETED */
+// ?product_id=61575&page=1&count=5
+// url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/qa/questions/?${new URLSearchParams({
+//   product_id,
+//   page,
+//   count,
+// })}`,
+// headers: { Authorization: process.env.TOKEN },
