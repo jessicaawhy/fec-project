b948c1b98b174b654af39822784509728f45d8f2
Jessica Yang <jessicaawhy@gmail.com>
2021-11-07 23:47:20 -0800

    Fix bug with hooks

--- client/components/Reviews/components/ReviewsComponent.jsx
+++ client/components/Reviews/components/ReviewsComponent.jsx
@@ -19,6 +19,14 @@ const ReviewsComponent = () => {
   const [subset, setSubset] = useState(null);
   const [meta, setMeta] = useState(null);
 
+  useEffect(() => {
+    setProduct(current);
+  }, [current]);
+
+  useEffect(() => {
+    setSort('relevance');
+  }, [product]);
+
   useEffect(async () => {
     const [updatedReviews, updatedMeta] = await Promise.all([
       getData(product.id, 1, 100, sort), getMetaData(product.id),
@@ -29,15 +37,7 @@ const ReviewsComponent = () => {
     setFilter({});
 
     isInitialMount.current = false;
-  }, [sort]);
-
-  useEffect(() => {
-    setProduct(current);
-  }, [current]);
-
-  useEffect(() => {
-    setSort('relevance');
-  }, [product]);
+  }, [sort, product]);
 
   useEffect(() => {
     if (!isInitialMount.current) {
