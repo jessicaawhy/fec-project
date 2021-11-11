12a145b7e7f401048e9ed72363c88f4df2fca285
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-10 19:40:42 -0800

    Fix style selector

--- client/components/overview/StyleSelector.jsx
+++ client/components/overview/StyleSelector.jsx
@@ -1,31 +1,21 @@
-import React, { useState, useEffect } from 'react';
-// import map from 'lodash/map';
+import React from 'react';
 import PropTypes from 'prop-types';
 import StyleButton from './StyleButton';
 import { StyleSelectorContainer } from './styles/StyleSelector.style';
 
 const StyleSelector = ({ productStyles, currentStyle, setCurrentStyle }) => {
-  useEffect(() => {
-    console.log('-------------- useEffect() StyleSelector is running.... //');
-    console.log('CurrentStyle: ', currentStyle);
-  }, [currentStyle]);
-
   const renderStyleButtons = () => productStyles.map((style) => {
     const styleId = style.style_id;
     const { photos } = style;
 
     return (
-      <>
-        <StyleButton
-          styleId={styleId}
-          photos={photos}
-          currentStyle={currentStyle}
-          setCurrentStyle={setCurrentStyle}
-          productStyles={productStyles}
-        />
-
-        {console.log('-------------- currentStyle StyleSelector running... ', currentStyle)}
-      </>
+      <StyleButton
+        styleId={styleId}
+        photos={photos}
+        currentStyle={currentStyle}
+        setCurrentStyle={setCurrentStyle}
+        productStyles={productStyles}
+      />
     );
   });
 
