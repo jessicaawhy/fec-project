7629cecc33b765945211c821301eafd6927ffd96
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-10 19:42:40 -0800

    Fix product info

--- client/components/overview/ProductInfo.jsx
+++ client/components/overview/ProductInfo.jsx
@@ -1,43 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {
   ProductInfoContainer, CategoryStyle, NameStyle, PriceStyle,
 } from './styles/ProductInfo.style';
 
-const ProductInfo = ({ productInfo, currentStyle }) => {
-  const [displayedStyle, setDisplayedStyle] = useState(currentStyle);
-
-  useEffect(() => {
-    console.log('-------------- useEffect() ProductInfo running... //');
-    console.log('CurrentStyle: ', currentStyle);
-    setDisplayedStyle(currentStyle);
-  }, [currentStyle]);
-
-  return (
-    <ProductInfoContainer data-testid="product-info">
-      <CategoryStyle>{productInfo.category}</CategoryStyle>
-      <NameStyle>{productInfo.name}</NameStyle>
-      {/* <> */}
-      {(currentStyle.sale_price !== null)
-        // need to add strikethrough of original price if sale price
-        ? (
-          <PriceStyle>
-            $
-            {currentStyle.sale_price}
-            <strike>{currentStyle.original_price}</strike>
-            {console.log('-------------- currentStyle ProductInfo running... ', currentStyle)}
-          </PriceStyle>
-        )
-        : (
-          <PriceStyle>
-            $
-            {currentStyle.original_price}
-          </PriceStyle>
-        )}
-      {/* </> */}
-    </ProductInfoContainer>
-  );
-};
+const ProductInfo = ({ productInfo, currentStyle }) => (
+  <ProductInfoContainer data-testid="product-info">
+    <CategoryStyle>{productInfo.category}</CategoryStyle>
+    <NameStyle>{productInfo.name}</NameStyle>
+    {(currentStyle.sale_price !== null)
+      ? (
+        <PriceStyle>
+          $
+          {currentStyle.sale_price}
+          <strike>{currentStyle.original_price}</strike>
+        </PriceStyle>
+      )
+      : (
+        <PriceStyle>
+          $
+          {currentStyle.original_price}
+        </PriceStyle>
+      )}
+  </ProductInfoContainer>
+);
 
 export default ProductInfo;
 ProductInfo.propTypes = PropTypes.shape({
