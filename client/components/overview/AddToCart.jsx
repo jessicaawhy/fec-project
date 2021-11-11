e0965df5995cdebf1c1d7a3ac25d515553df25d6
Olivia Schwerdtfeger <okschwerdtfeger@gmail.com>
2021-11-10 19:51:09 -0800

    Add comment in AddToCart for tech debt task

--- client/components/overview/AddToCart.jsx
+++ client/components/overview/AddToCart.jsx
@@ -6,6 +6,7 @@ import { addProductToCart } from './helpers/api';
 import { CartContainer, CartButton, DropDown } from './styles/AddToCart.style';
 
 const AddToCart = ({ currentStyle }) => {
+  // TODOS: make dropdowns reset when a new product or style is selected
   const [currentSize, setCurrentSize] = useState('Size');
   const [currentQuantity, setCurrentQuantity] = useState('Quantity');
   const [currentSku, setCurrentSku] = useState(null);
