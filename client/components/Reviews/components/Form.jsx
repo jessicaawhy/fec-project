3e2681a1ea2bbb34c7a9aa166eaddc03b2915eae
Jessica Yang <jessicaawhy@gmail.com>
2021-11-13 00:48:35 -0800

    Refactor characteristics in Form component

--- client/components/Reviews/components/Form.jsx
+++ client/components/Reviews/components/Form.jsx
@@ -8,6 +8,7 @@ import {
 import Button from '../../styles/Button.styled';
 import StyledForm from '../styles/Form.styled';
 import { submitForm } from '../helpers/helpers';
+import formChars from '../data/characteristics';
 
 const ReviewsForm = ({ meta }) => {
   const [submitted, setSubmitted] = useState(false);
@@ -51,7 +52,6 @@ const ReviewsForm = ({ meta }) => {
     )
     : (
       <StyledForm onSubmit={handleSubmit}>
-
         <StarRating />
         <Name />
         <Email />
@@ -59,77 +59,15 @@ const ReviewsForm = ({ meta }) => {
         <Body />
         <Recommend />
 
-        <Characteristics
-          name="size"
-          mainLabel="Size"
-          elements={[
-            'A size too small',
-            '1/2 a size too small',
-            'Perfect',
-            '1/2 a size too big',
-            'A size too big',
-          ]}
-        />
-
-        <Characteristics
-          name="width"
-          mainLabel="Width"
-          elements={[
-            'Too narrow',
-            'Slightly narrow',
-            'Perfect',
-            'Slightly wide',
-            'Too wide',
-          ]}
-        />
-
-        <Characteristics
-          name="comfort"
-          mainLabel="Comfort"
-          elements={[
-            'Uncomfortable',
-            'Slightly uncomfortable',
-            'Ok',
-            'Comfortable',
-            'Perfect',
-          ]}
-        />
-
-        <Characteristics
-          name="quality"
-          mainLabel="Quality"
-          elements={[
-            'Poor',
-            'Below Average',
-            'Expected',
-            'Pretty Great',
-            'Perfect',
-          ]}
-        />
-
-        <Characteristics
-          name="length"
-          mainLabel="Length"
-          elements={[
-            'Runs short',
-            'Runs slightly short',
-            'Perfect',
-            'Runs slightly long',
-            'Runs long',
-          ]}
-        />
-
-        <Characteristics
-          name="fit"
-          mainLabel="Fit"
-          elements={[
-            'Runs tight',
-            'Runs slightly tight',
-            'Perfect',
-            'Runs slightly loose',
-            'Runs loose',
-          ]}
-        />
+        {
+          Object.keys(formChars).map((char) => (
+            <Characteristics
+              name={char}
+              mainLabel={char[0].toUpperCase() + char.slice(1)}
+              elements={formChars[char]}
+            />
+          ))
+        }
 
         {/* TODO: Upload your photos */}
 
